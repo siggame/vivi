fefbd08b8cc4cf0aea80b43575a783f1d14dd6a2
user404d <conduffql@gmail.com>
2017-10-03 21:53:18 -0500

    Add GUILD_NAME env var for use in bot

--- src/vars.ts
+++ src/vars.ts
@@ -1,2 +1,3 @@
-export const TOKEN: string = process.env.TOKEN || "";
+export const GUILD_NAME: string = process.env.GUILD_NAME || "ACM SIG-Game";
 export const PREFIX: string = process.env.PREFIX || "!";
+export const TOKEN: string = process.env.TOKEN || "";
