bf7bcd686c0e19503c09bd65c96e8aaa9ff00d2a
Reno DuBois <renodubois@users.noreply.github.com>
2017-11-13 18:28:56 -0600

    Initial update implementation

--- src/vars.ts
+++ src/vars.ts
@@ -1,3 +1,6 @@
 export const GUILD_NAME: string = process.env.GUILD_NAME || "ACM SIG-Game";
 export const PREFIX: string = process.env.PREFIX || "!";
 export const TOKEN: string = process.env.TOKEN || "";
+export const STATUS_REPO_NAME: string = process.env.STATUS_REPO_NAME || "status";
+export const STATUS_REPO_OWNER: string = process.env.STATUS_REPO_OWNER || "siggame";
+export const STATUS_GITHUB_TOKEN: string = process.env.STATUS_GITHUB_TOKEN || "";
