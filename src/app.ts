01bdedb60b3f4cefab31e0b428acc4e1005715c8
Dylan warren <duwtgb@mst.edu>
2018-02-24 01:29:32 -0600

    Removed the silly shit from Vivi, time to make vivi worth something

--- src/app.ts
+++ src/app.ts
@@ -12,26 +12,6 @@ import { STATUS_CHANNEL_ID, PREFIX, TOKEN } from "./vars";
 
 const client = new Discord.Client();
 
-const iRuNlInUX = `I'd just like to interject for a moment.  What you're referring to as Linux,
-is in fact, GNU/Linux, or as I've recently taken to calling it, GNU plus Linux.
-Linux is not an operating system unto itself, but rather another free component
-of a fully functioning GNU system made useful by the GNU corelibs, shell
-utilities and vital system components comprising a full OS as defined by POSIX.
-
-Many computer users run a modified version of the GNU system every day,
-without realizing it.  Through a peculiar turn of events, the version of GNU
-which is widely used today is often called \"Linux\", and many of its users are
-not aware that it is basically the GNU system, developed by the GNU Project.
-
-There really is a Linux, and these people are using it, but it is just a
-part of the system they use.  Linux is the kernel: the program in the system
-that allocates the machine's resources to the other programs that you run.
-The kernel is an essential part of an operating system, but useless by itself;
-it can only function in the context of a complete operating system.  Linux is
-normally used in combination with the GNU operating system: the whole system
-is basically GNU with Linux added, or GNU/Linux.  All the so-called "Linux"
-distributions are really distributions of GNU/Linux.`;
-
 // Silly "Playing with..." thing.
 client.on("ready", () => {
   console.log(`[Started] ${new Date()}`);
@@ -41,39 +21,11 @@ client.on("ready", () => {
   client.user.setGame(" with life");
 });
 
-client.on("message", (message: Discord.Message) => {
-  let mes: string = message.content.toLowerCase();  
-  if(mes !== "thanks vivi" && mes !== "thank you vivi") return;
- 
-  let thanks: string[] = [
-    "No problem!",
-    "I've got your back!",
-    "It was nothing ;p",
-    "I-i-it's not like I wanted to help you... baka! ;_;",
-    "Don't thank me, thank my creator!",
-    "My pleasure.",
-    "Sure thing.",
-    "Think nothing of it.",
-    "Happy to help!"
-  ];
- 
-  let response: string = thanks[Math.floor(Math.random()*thanks.length)];
-  message.channel.send(response);
-});
-
 // Vivi reads in messages and checks them against our responseObject to give a sweet reply
 client.on("message", (message: Discord.Message) => {
   if (responseObject.has(message.content.toLowerCase())) {
     message.channel.send(responseObject.get(message.content.toLowerCase()));
   }
-
-  //BECAUSE OF MAYMAYS MY DOOOOOOOD
-  const words = message.content.toLowerCase();
-  if (words.includes("linux") && !(message.author.bot) && (Math.random() * 1) < 0.05) {
-    message.channel.send(iRuNlInUX);
-    return;
-  }
-
 });
 
 // This will most likely be the main part of Vivi (for prefix maymays n stuff)
