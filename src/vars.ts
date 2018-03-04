b85d9ddd492d9f5d792e9a1dbe4e3f276551c06e
Dylan warren <duwtgb@mst.edu>
2018-03-03 20:42:47 -0600

    Should've stayed lowercase, my bad

--- src/vars.ts
+++ src/vars.ts
@@ -1,7 +1,7 @@
 export const GUILD_NAME: string = process.env.GUILD_NAME || "ACM SIG-Game";
 export const PREFIX: string = process.env.PREFIX || "!";
 export const TOKEN: string = process.env.TOKEN || "";
-export const STATUS_REPO_NAME: string = process.env.STATUS_REPO_NAME || "Status";
+export const STATUS_REPO_NAME: string = process.env.STATUS_REPO_NAME || "status";
 export const STATUS_REPO_OWNER: string = process.env.STATUS_REPO_OWNER || "siggame";
 export const STATUS_GITHUB_TOKEN: string = process.env.STATUS_GITHUB_TOKEN || "";
 export const STATUS_CHANNEL_ID: string = process.env.STATUS_CHANNEL_ID || "275704695568334848";
